10991bbc833796382b55d0a1cc35cbe41b390c8c
LadyPinkElf-Elysia <3173382688@qq.com>
2026-05-11 22:16:45 +0800

    移除全局样式中的内边距，并添加音频和图像源的注释

--- config.js
+++ config.js
@@ -17,48 +17,95 @@ export const RATE_MAP = {
 
 export const PURPLE_RATE = 0.20
 
+//https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game
+
 export const AUDIO_SRC = {
-    '1': { 'name': '蝶恋花', 'src': "./music/蝶恋花.mp3" },
-    '2': { 'name': '世末歌者', 'src': "./music/世末歌者.ogg" },
-    '3': { 'name': '千本樱', 'src': "./music/千本樱.mp3" },
-    '4': { 'name': '大荒寻梦录', 'src': "./music/大荒寻梦录.mp3" },
-    '5': { 'name': '群青', 'src': "./music/群青.ogg" },
-    '6': { 'name': '浴火重生', 'src': "./music/浴火重生.ogg" },
-    '7': { 'name': '英雄出征', 'src': "./music/英雄出征.ogg" },
-    '8': { 'name': '非人哉', 'src': "./music/非人哉.mp3" },
-    '9': { 'name': '熙熙攘攘我们的城市', 'src': "./music/熙熙攘攘我们的城市.mp3" },
-    '10': { 'name': '红昭愿', 'src': "./music/红昭愿.mp3" },
-    '11': { 'name': '我的悲伤是水做的', 'src': "./music/我的悲伤是水做的.ogg" },
+    '1': { 'name': '蝶恋花', 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/music/蝶恋花.mp3" },
+    '2': { 'name': '世末歌者', 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/music/世末歌者.ogg" },
+    '3': { 'name': '千本樱', 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/music/千本樱.mp3" },
+    '4': { 'name': '大荒寻梦录', 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/music/大荒寻梦录.mp3" },
+    '5': { 'name': '群青', 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/music/群青.ogg" },
+    '6': { 'name': '浴火重生', 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/music/浴火重生.ogg" },
+    '7': { 'name': '英雄出征', 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/music/英雄出征.ogg" },
+    '8': { 'name': '非人哉', 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/music/非人哉.mp3" },
+    '9': { 'name': '熙熙攘攘我们的城市', 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/music/熙熙攘攘我们的城市.mp3" },
+    '10': { 'name': '红昭愿', 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/music/红昭愿.mp3" },
+    '11': { 'name': '我的悲伤是水做的', 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/music/我的悲伤是水做的.ogg" },
+    '12': { 'name': 'TruE', 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/music/TruE.mp3" },
 }
 
 export const IMAGE_SRC = {
-    '1': { 'src': "./img/小奥-希望人没事.png" },
-    '2': { 'src': "./img/大眼七夕封面.png" },
-    '3': { 'src': "./img/小诗.png" },
-    '4': { 'src': "./img/小诗心链.png" },
-    '5': { 'src': "./img/尼娅.png" },
-    '6': { 'src': "./img/啥乌.png" },
-    '7': { 'src': "./img/洛天依.png" },
-    '8': { 'src': "./img/游乐园小诗.png" },
-    '9': { 'src': "./img/落水爱莉.png" },
+    '1': { 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/img/小奥-希望人没事.png" },
+    '2': { 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/img/大眼七夕封面.png" },
+    '3': { 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/img/小诗.png" },
+    '4': { 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/img/小诗心链.png" },
+    '5': { 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/img/尼娅.png" },
+    '6': { 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/img/啥乌.png" },
+    '7': { 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/img/洛天依.png" },
+    '8': { 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/img/游乐园小诗.png" },
+    '9': { 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/img/落水爱莉.png" },
 }
 
 export const ELYSIA_IMAGE_SRC = {
-    '1': { 'name': "加载中...", 'src': "./elysiaImg/爱莉揣小手.png" },
-    '2': { 'name': "经典模式", 'src': "./elysiaImg/爱莉2.png" },
-    '3': { 'name': "自定义模式", 'src': "./elysiaImg/爱莉9.png" },
-    '4': { 'name': "游戏规则", 'src': "./elysiaImg/爱莉.png" },
-    '5': { 'name': "游戏提示", 'src': "./elysiaImg/爱莉10.png" },
-    '6': { 'name': "游戏胜利", 'src': "./elysiaImg/爱莉点赞.png" },
-    '7': { 'name': "游戏失败", 'src': "./elysiaImg/爱莉游戏失败.png" },
-    '8': { 'name': "游戏回放", 'src': "./elysiaImg/爱莉翻花绳.png" },
+    '1': { 'name': "加载中...", 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/elysiaImg/爱莉揣小手.png" },
+    '2': { 'name': "经典模式", 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/elysiaImg/爱莉2.png" },
+    '3': { 'name': "自定义模式", 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/elysiaImg/爱莉9.png" },
+    '4': { 'name': "游戏规则", 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/elysiaImg/爱莉.png" },
+    '5': { 'name': "游戏提示", 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/elysiaImg/爱莉10.png" },
+    '6': { 'name': "游戏胜利", 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/elysiaImg/爱莉点赞.png" },
+    '7': { 'name': "游戏失败", 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/elysiaImg/爱莉游戏失败.png" },
+    '8': { 'name': "游戏回放", 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/elysiaImg/爱莉翻花绳.png" },
 }
 
 export const CLASSIC_IMG = {
-    'easy': { 'src': "./elysiaImg/爱莉3.png" },
-    'hard': { 'src': "./elysiaImg/爱莉4.png" },
-    'hell': { 'src': "./elysiaImg/爱莉8.png" },
+    'easy': { 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/elysiaImg/爱莉3.png" },
+    'hard': { 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/elysiaImg/爱莉4.png" },
+    'hell': { 'src': "https://cdn.jsdelivr.net/gh/ladypinkelf-elysia/guess-number-game/elysiaImg/爱莉8.png" },
 }
+
+// export const AUDIO_SRC = {
+//     '1': { 'name': '蝶恋花', 'src': "./music/蝶恋花.mp3" },
+//     '2': { 'name': '世末歌者', 'src': "./music/世末歌者.ogg" },
+//     '3': { 'name': '千本樱', 'src': "./music/千本樱.mp3" },
+//     '4': { 'name': '大荒寻梦录', 'src': "./music/大荒寻梦录.mp3" },
+//     '5': { 'name': '群青', 'src': "./music/群青.ogg" },
+//     '6': { 'name': '浴火重生', 'src': "./music/浴火重生.ogg" },
+//     '7': { 'name': '英雄出征', 'src': "./music/英雄出征.ogg" },
+//     '8': { 'name': '非人哉', 'src': "./music/非人哉.mp3" },
+//     '9': { 'name': '熙熙攘攘我们的城市', 'src': "./music/熙熙攘攘我们的城市.mp3" },
+//     '10': { 'name': '红昭愿', 'src': "./music/红昭愿.mp3" },
+//     '11': { 'name': '我的悲伤是水做的', 'src': "./music/我的悲伤是水做的.ogg" },
+//     '12': { 'name': 'TruE', 'src': "./music/TruE.mp3" },
+// }
+
+// export const IMAGE_SRC = {
+//     '1': { 'src': "./img/小奥-希望人没事.png" },
+//     '2': { 'src': "./img/大眼七夕封面.png" },
+//     '3': { 'src': "./img/小诗.png" },
+//     '4': { 'src': "./img/小诗心链.png" },
+//     '5': { 'src': "./img/尼娅.png" },
+//     '6': { 'src': "./img/啥乌.png" },
+//     '7': { 'src': "./img/洛天依.png" },
+//     '8': { 'src': "./img/游乐园小诗.png" },
+//     '9': { 'src': "./img/落水爱莉.png" },
+// }
+
+// export const ELYSIA_IMAGE_SRC = {
+//     '1': { 'name': "加载中...", 'src': "./elysiaImg/爱莉揣小手.png" },
+//     '2': { 'name': "经典模式", 'src': "./elysiaImg/爱莉2.png" },
+//     '3': { 'name': "自定义模式", 'src': "./elysiaImg/爱莉9.png" },
+//     '4': { 'name': "游戏规则", 'src': "./elysiaImg/爱莉.png" },
+//     '5': { 'name': "游戏提示", 'src': "./elysiaImg/爱莉10.png" },
+//     '6': { 'name': "游戏胜利", 'src': "./elysiaImg/爱莉点赞.png" },
+//     '7': { 'name': "游戏失败", 'src': "./elysiaImg/爱莉游戏失败.png" },
+//     '8': { 'name': "游戏回放", 'src': "./elysiaImg/爱莉翻花绳.png" },
+// }
+
+// export const CLASSIC_IMG = {
+//     'easy': { 'src': "./elysiaImg/爱莉3.png" },
+//     'hard': { 'src': "./elysiaImg/爱莉4.png" },
+//     'hell': { 'src': "./elysiaImg/爱莉8.png" },
+// }
 
 export const GAME_DEFAULTS = {
     mode: {
